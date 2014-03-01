17df55b8689e431569550719177121395e61348c
smore9982 <sameermore412@gmail.com>
2014-03-01 16:21:55 -0500

    Initial Commit

---
+++ CanIMakeItGTFSUploader/src/com/canimakeituploader/Uploader.java
@@ -0,0 +1,13 @@
+package com.canimakeituploader;
+
+import java.io.File;
+
+public class Uploader {
+	
+	public static void main(String[] args) throws Exception {
+		String fileName = args[0];
+		File f = new File(fileName);
+		
+	}
+
+}
